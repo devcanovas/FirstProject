34ec5d126c2d20c708af2b88b1d26728a57ec4ae
Matheus Canovas <matheuscanovas9@gmail.com>
2024-02-19 17:38:13 -0300

    feat: Implementing band register and list

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Security.Cryptography;
 
-string welcomeMessage = "Bosa vindas ao Screen Sound";
+string welcomeMessage = "Boas vindas ao Screen Sound";
 List<string> bandList = new List<string> { "Bring Me The Horizon", "Slipknot", "Linkin Park" };
 
 void ShowLogo()
@@ -37,17 +37,54 @@ void handleOption(int opt)
 {
     switch (opt)
     {
-        case 1: Console.WriteLine("Você escolheu a opção 1!");
+        case 1:
+            InputBands();
             break;
-        case 2: Console.WriteLine("Você escolheu a opção 2!");
+        case 2:
+            ShowBands();
             break;
-        case 3: Console.WriteLine("Você escolheu a opção 3!");
+        case 3:
+            Console.WriteLine("Você escolheu a opção 3!");
             break;
-        case 4: Console.WriteLine("Você escolheu a opção 4!");
+        case 4:
+            Console.WriteLine("Você escolheu a opção 4!");
             break;
-        case -1: Console.WriteLine("Tchau tchau!");
+        case -1:
+            Console.WriteLine("Tchau tchau!");
             break;
-        default: Console.WriteLine("Opção inválida!");
+        default:
+            Console.WriteLine("Opção inválida!");
             break;
     }
 }
+
+void InputBands()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de bandas");
+    Console.WriteLine("Digite o nome da banda que deseja registrar");
+    string bandName = Console.ReadLine()!;
+    bandList.Add(bandName);
+    Console.WriteLine($"A banda {bandName} foi registrada com sucesso!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ShowMenuOptions();
+}
+
+void ShowBands()
+{
+    Console.Clear();
+    Console.WriteLine("**********************************************");
+    Console.WriteLine("EXIBINDO LISTA DE NOMES DAS BANDAS REGISTRADAS");
+    Console.WriteLine("**********************************************\n");
+    bandList.ForEach(delegate (string bandName)
+    {
+        Console.WriteLine($"- {bandName}");
+    });
+    Console.WriteLine("\nPressiona ENTER para voltar ao menu inicial!");
+    Console.ReadKey();
+    Console.Clear();
+    ShowMenuOptions();
+}
+
+ShowMenuOptions();
--- obj/Debug/net8.0/FirstProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/FirstProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13774128104c492c636c68955a5de0a333a70080")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
